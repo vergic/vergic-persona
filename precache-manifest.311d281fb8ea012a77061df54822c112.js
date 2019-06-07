0051476756184bcb01851028ff99a6128535531c
Rezmodeus <rezmodeus@yahoo.com>
2019-06-07 09:43:48 +0200

    Updates

---
+++ precache-manifest.311d281fb8ea012a77061df54822c112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10935d2cc89844b792f762080b2c759c",
+    "url": "/vergic-persona/index.html"
+  },
+  {
+    "revision": "e3b52364a58e0a1467aa",
+    "url": "/vergic-persona/static/css/2.20d67eb1.chunk.css"
+  },
+  {
+    "revision": "8124074b6ebf88f2c72a",
+    "url": "/vergic-persona/static/css/main.a7127ebe.chunk.css"
+  },
+  {
+    "revision": "e3b52364a58e0a1467aa",
+    "url": "/vergic-persona/static/js/2.744fea6a.chunk.js"
+  },
+  {
+    "revision": "8124074b6ebf88f2c72a",
+    "url": "/vergic-persona/static/js/main.7d92520e.chunk.js"
+  },
+  {
+    "revision": "f34a39f95b8ca8287083",
+    "url": "/vergic-persona/static/js/runtime~main.46a062a3.js"
+  }
+]);
